95c87f5a8ed763eb94ee2d102c45451f91d93b7a
samuelsky <samuelebflithy@gmail.com>
2026-05-21 16:04:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from datetime import timedelta
 import json
 import os
 
+# ===== WEB SERVER =====
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot online!"
+
+def run():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 # =========================================================
 # CONFIG
 # =========================================================
